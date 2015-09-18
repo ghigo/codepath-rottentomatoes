189e5e8962ca90c02c695ff6523cd46d53cf450c
Marco Sgrignuoli <marco.sgrignuoli@gmail.com>
2015-09-17 19:31:44 -0700

    Refine details view

--- rotten tomatoes/detailsViewController.swift
+++ rotten tomatoes/detailsViewController.swift
@@ -20,8 +20,29 @@ class detailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        backgroundImage.setImageWithURL(<#T##url: NSURL##NSURL#>)
+//        backgroundImage.setImageWithURL()
+        
+//        URL stuff
+        var urlString = selectedMovie?.valueForKeyPath("posters.thumbnail") as! String
+        
+//        Edit url for higher res images
+        let range = urlString.rangeOfString(".*cloudfront.net/", options: .RegularExpressionSearch)
+        if let range = range {
+            urlString = urlString.stringByReplacingCharactersInRange(range, withString: "https://content6.flixster.com/")
+        }
+        let imageUrl = NSURL(string: urlString)
+        backgroundImage.setImageWithURL(imageUrl!)
+        
         titleLabel.text = selectedMovie?.valueForKeyPath("title") as! String
+        
+        synopsisLabel.text = selectedMovie?.valueForKeyPath("synopsis") as! String
+        
+//        let year_ = NSString(string: selectedMovie?.valueForKeyPath("year") as! Int)
+        
+        let year = String(format: "%d", (selectedMovie?.valueForKeyPath("year") as! Int))
+        let aScore = String(format: "%d", (selectedMovie?.valueForKeyPath("ratings.audience_score"))as! Int)
+        let cScore = String(format: "%d", (selectedMovie?.valueForKeyPath("ratings.critics_score") as! Int))
+        detailsLabel.text = "Year: \(year), Critics score: \(cScore), Audience score: \(aScore)"
     }
 
     override func didReceiveMemoryWarning() {
